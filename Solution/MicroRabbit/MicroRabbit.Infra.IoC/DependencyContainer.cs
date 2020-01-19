09ccd2f11cad23f5d5b60f870515f1b7ba49b14c
James C. Posey <indyy66@hotmail.com>
2020-01-18 17:02:31 -0800

    Initial commit of infrastructure dependency project

---
+++ Solution/MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,24 @@
+﻿
+namespace MicroRabbit.Infra.IoC
+{
+    using MicroRabbit.Domain.Core.Bus;
+    using MicroRabbit.Infra.Bus;
+    using Microsoft.Extensions.DependencyInjection;
+
+
+    /// <summary>
+    /// IoC Dependency injection management class
+    /// </summary>
+    public class DependencyContainer
+    {
+        /// <summary>
+        /// Registers the services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
